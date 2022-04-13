b6636d31bf8ae089fb639986585d274d3284c2c2
Junya-Shimada <s1280167@u-aizu.ac.jp>
2022-04-14 01:33:26 +0900

    [4]

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ Vue.use(Router);
 export default new Router({
     mode: "history",
     routes: [
-        {path: '/', component: Top},
+        {path: '/cocktail-maker/', component: Top},
         {path: '/addition', component: Addition},
         {path: '/test', component: Test},
         {path: '/list', component: List},
